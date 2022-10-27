c472ea6ee7832bbb844abbfc6ebfe64bf5acfae7
charaf marghin <developermarghin@gmail.com>
2022-10-27 15:33:41 +0100

    change default colors of daisyui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,5 +4,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        marhaba: {
+          primary: "#FBBF24",
+          "primary-text": "#272935",
+          secondary: "#3B82F6",
+          "secondary-text": "#333",
+          accent: "#F87171",
+          "accent-text": "#333",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
