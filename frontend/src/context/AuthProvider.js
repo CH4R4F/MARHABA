567ac700e43e6328915ed4908a20d3334794d084
charaf marghin <developermarghin@gmail.com>
2022-10-27 14:44:38 +0100

    add initial auth context

---
+++ frontend/src/context/AuthProvider.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider };
+export default AuthContext;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthProvider } from "./context/AuthProvider";
 import "./index.css";
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
 rootElement.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
