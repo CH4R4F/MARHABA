671d5d4243ecd22927c771972249ccc4624476c8
charaf marghin <developermarghin@gmail.com>
2022-10-29 15:20:34 +0100

    remove unused useEffect

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Input from "../components/common/Input";
 import InputValidation from "../utils/InputValidation";
