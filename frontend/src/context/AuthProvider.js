e73ae512a64753c6978ff949ebfd843ccb142785
charaf marghin <developermarghin@gmail.com>
2022-11-16 08:47:55 +0100

    add dashboard

--- frontend/src/context/AuthProvider.js
+++ frontend/src/context/AuthProvider.js
@@ -1,15 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
 
-  return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (token && user) {
+      setAuth({ token, user });
+    }
+  }, []);
+
+  return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
 };
 
 export { AuthProvider };
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  // centered text with some test text
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-4xl">Dashboard</h1>
+        <p className="text-xl">This is the dashboard page</p>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
